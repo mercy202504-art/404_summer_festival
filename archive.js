1e80bd4b85584e8a11abb9dbdf13c507465af0e5
mercy202504-art <mercy2025.04@gmail.com>
2026-07-18 20:26:02 +0900

    Update archive.js

--- archive.js
+++ archive.js
@@ -267,6 +267,13 @@ setTimeout(() => {
 
 },5200);
 
+setTimeout(() => {
+const image = document.createElement("img");
+
+image.src = "images/memory145-v2.png";
+image.className = "completion-image";
+image.alt = "Last Observer";
+
 const panel =
     overlay.querySelector(".completion-panel");
 
