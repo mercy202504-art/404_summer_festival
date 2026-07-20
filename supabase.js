1aaee27758d8062d97f197bb8323b2c9c1dd5703
mercy202504-art <mercy2025.04@gmail.com>
2026-07-20 10:24:12 +0900

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,11 +2,12 @@ const SUPABASE_URL = "https://mzmhbeqvdlaokzjalmxa.supabase.co";
 
 const SUPABASE_KEY = "sb_publishable_aRH1j7nIKa8dnL5tKR1dHQ_0qqH2a6f";
 
-const supabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
 );
-console.log("Supabase connected!", supabase);
+
+console.log("Supabase connected!", supabaseClient);
 
 async function updateConnectionRecord() {
   const countElement = document.getElementById("connection-count");
