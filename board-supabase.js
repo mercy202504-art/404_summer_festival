4b9935c42a3f55a65aab36f6c92e8bd64040726a
mercy202504-art <mercy2025.04@gmail.com>
2026-07-31 05:56:57 +0900

    supabase

---
+++ board-supabase.js
@@ -0,0 +1,10 @@
+const SUPABASE_URL = "https://mzmhbeqvdlaokzjalmxa.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_aRH1j7nIKa8dnL5tKR1dHQ_0qqH2a6f";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
+
+console.log("Board Supabase connected!", supabaseClient);
